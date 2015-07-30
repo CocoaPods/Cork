b13650fa55a61d2e92501b716113385dc3ba6363
Karla-Sandoval-Sandoval <k.isabel.sandoval@gmail.com>
2015-07-30 10:39:03 -0700

    Writing tests for board.rb

--- lib/cork/board.rb
+++ lib/cork/board.rb
@@ -44,10 +44,10 @@ module Cork
       input.gets
     end
 
-    # Stores important warming to the user optionally followed by actions that the user should take. To print them use {#print_warnings}.
+    # Stores important warning to the user optionally followed by actions that the user should take. To print them use {#print_warnings}.
     #
     # @param [String] message The message to print.
-      @param [Array]  actions The actions that the user should take.
+    #  @param [Array]  actions The actions that the user should take.
     #
     # return [void]
     #
@@ -55,5 +55,68 @@ module Cork
       warnings << { :message =>, :actions  => actions, :verbose_only => }
     end
 
+    def path(pathname, relative_to = Pathname.cpwd)
+      if pathname
+      path = Pathname(pathname).relative_path_from(from_path)
+      "`#{{path}}`"
+    else
+      ''
+    end
+  end
+
+  def labeled(label,value,justification = 12)
+    if value
+      title = "- #{label}:"
+      if value.is_a?(Enumerable)
+        lines = [wrap_string(title, self.indentation_level)]
+        value.each do |v|
+          lines << wrap_string "- #{v}", self.indentation_level + 2
+        end
+        puts lines.join("\n")
+      else
+        puts wrap_string(title.ljust(justification) + "#{value}", self.indentation_level)
+      end
+    end
+  end
+
+
+
+
+
+
+
+
+
+
+
+
+
     private
 end
+    # @!group Helpers
+    #-------------------------------------------------------------------#
+
+    # @return [String] Wraps a string taking into account the width of the
+    # terminal and an option indent. Adapted from http://blog.macromates.com/2006/wrapping-text-with-regular-expressions/
+    #
+    # @param [String] txt The string to wrap=
+    #
+    # @param [String] indent The string to use to indent the result.
+    #
+    # @return [String]    The formatted string.
+    #
+    # @note If Cork is not being run in a terminal or the width of the
+    #terminal is to samll a width of 80 is assumed.
+    #
+    def wrap_string(string, indent =0)
+      if disable_wrap
+        (' ' * indent) + string
+      else
+      first_space = ' ' *indent
+      indented = TextWrapper.warp_with_indent(string,indent,9999)
+      first_space + indented
+    end
+  end
+end
+end
+UI = UserInterface
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -11,6 +11,35 @@ module Cork
         board = Board.new(:verbose => true)
         board.should.be.verbose
       end
+
+        it 'allows specifying silent' do
+          board = Board.new(:silent => false)
+          board.should.not.be.silent
+
+        end
+
+          it 'allows specifying ansi' do
+          board = Board.new(:ansi => true)
+          board.should.be.ansi
+
+        end
+
+        it 'allows specifying input' do
+          board =Board.new(:input => $stdin)
+          board.stdin.should == $stdin
+
+        end
+
+        it 'allows specifying out' do
+        board = Board.new (:out => $stdout)
+        board.stdout.shoud == $stdout
+
+        end
+
+        it 'allows specifying err' do
+          board = Board.new (:err => $stderr)
+          board.stderr.should == $stderr
+        end
     end
 
     describe '#puts' do
@@ -19,13 +48,62 @@ module Cork
         @board.puts('abc')
       end
 
-      it 'call out#puts with an empty string when not silent' do
+      it 'calls out#puts with an empty string when not silent' do
         @board.out.expects(:puts).with('abc')
         @board.puts('abc')
       end
+
+      it 'does not call out#puts when silent' do
+        @board.stubs(:silent?).returns(true)
+        @board.out.expects(:puts).never
+        @board.puts('abc')
+        @board.puts
+      end
+    end
+
+    describe '#print' do
+      it 'calls out#print when not silent' do
+        @board.out.expects(:print).with('abc')
+        @board.print('abc')
+      end
+
+      it 'does not call out#print when silent' do
+      @board.stubs(:silent?).returns(true)
+      @board.out.expects(:print).never
+      @board.print('abc')
+      @board.print
+    end
+end
+
+    describe '#gets' do
+      it 'calls input#gets' do
+        @board.input.expects(:gets).returns('abc')
+        @board.gets.should == 'abc'
+      end
+
+      it 'does not call input#gets' do
+        @board.input.expects(:gets).returns('abc')
+        @board.gets.should == 'abc'
+        end
+    end
+
+    #---Check These Tests with Samuel. @param test are challenging--#
+
+          describe '#initialize' do
+            it 'allows passing an array of strings' do
+            board = Board.new(:param => [String])
+            board.should.be.passing.an.array.of.strings
+          end
+
+          it 'allows passing an array of arrays' do
+            board = Board.new(:parma => [Array])
+            board.should.be.passing.an.array.of.arrays
+          end
+
     end
   end
 end
+
 
 
       it does not call out#puts when silent

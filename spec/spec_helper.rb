deeecfea264288f4b2ab4b1751e4f7f0e2738596
Karla-Sandoval-Sandoval <k.isabel.sandoval@gmail.com>
2015-09-02 15:22:06 -0700

    Upload coverage reports to CodeCov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,10 @@
+require 'simplecov'
+SimpleCov.start
+if ENV['CI'] == 'true'
+  require 'codecov'
+  SimpleCov.formatter = SimpleCov::Formatter::Codecov
+end
+
 require 'pathname'
 ROOT = Pathname.new(File.expand_path('../../', __FILE__))
 $LOAD_PATH.unshift((ROOT + 'lib').to_s)
